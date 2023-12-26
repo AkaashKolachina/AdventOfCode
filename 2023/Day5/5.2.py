4f1f508879f389ac69caf1e46274be2a8f69e8f7
AkaashKolachina <54248406+AkaashKolachina@users.noreply.github.com>
2023-12-26 11:22:29 -0600

    Finally finish Day 5 Part 2 (using brute force)

--- 2023/Day5/5.2.py
+++ 2023/Day5/5.2.py
@@ -8,30 +8,12 @@ lines = [x for x in data.split('\n')]
 
 
 def convert_num(num, map):
-    for dst,src,step in map:
+    for src,dst,step in map:
         if num >= src and num < src + step:
             return (num - src) + dst
     return num
 
-def map_range(ranges, map):
-    proceessed = []
-    for dst,src,step in map:
-        buffer = []
-        for entry in ranges:
-            start = entry[0]
-            end = start + entry[1]
-            src_end = src + step
-            pre = (start,min(end,src))
-            overlap = (max(start, src), min(src_end, end))
-            post = (max(src_end, start), end)
-            if pre[1] > pre[0]:
-                buffer.append(pre)
-            if overlap[1] > overlap[0]:
-                proceessed.append((overlap[0]-src+dst, overlap[1]-src+dst))
-            if post[1] > post[0]:
-                buffer.append(post)
-        ranges = buffer
-    return ranges + proceessed
+
 
 
 seeds = []
@@ -43,15 +25,24 @@ for line in lines:
         seed_data = [int(x) for x in match.group(1).split()]
         seed_starts = [seed_data[i] for i in range(len(seed_data)) if i % 2 == 0]
         ranges = [seed_data[i] for i in range(len(seed_data)) if i % 2 != 0]
-        seeds = zip(seed_starts, ranges)
+        seeds = list(zip(seed_starts, ranges))
     elif "map" in line:
         maps.append([])
     elif line != '':
          matches = re.findall('\d+', line)
          maps[-1].append(tuple([int(x) for x in matches]))
 
-for seed in seeds:
-    ranges = [seed]
-    for map in maps:
-        ranges = map_range(ranges, map)
-    print(min(ranges))
+
+rev_maps = maps[::-1]
+min_location = 0
+found_min_loc = False
+while not found_min_loc:
+    val = min_location
+    for map in rev_maps:
+        val = convert_num(val, map)
+    for seed_start, seed_range in seeds:
+        if seed_start <= val < seed_start + seed_range:
+            print(min_location)
+            found_min_loc = True
+            break
+    min_location += 1
