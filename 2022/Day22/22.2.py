d754a3e3f08da0fa233d1635cc3ba38445388ab4
AkaashKolachina <54248406+AkaashKolachina@users.noreply.github.com>
2024-01-03 19:50:54 -0600

    Finally finish 2022 day 22

--- 2022/Day22/22.2.py
+++ 2022/Day22/22.2.py
@@ -5,45 +5,80 @@ from collections import defaultdict
 def wrap():
     if faces[cur] == 1:
         if dir_chars[dir] == 'L':
-            if cur[0] < (face_bounds[1][0][0] + 25):
-                new_r = (face_bounds[4][2][0] - (cur[0] - face_bounds[1][0][0]))
-                new_c = face_bounds[4][2][1]
-                return (new_r,new_c), 0
-            else:
-                new_r = (face_bounds[4][0][0] + (face_bounds[1][2][0] - cur[0]))
-                new_c = face_bounds[4][0][1]
-                return (new_r,new_c), 0
+            diff = face_bounds[1][2][0] - cur[0]
+            new_r = face_bounds[4][0][0] + diff
+            new_c = face_bounds[4][0][1]
+            return (new_r,new_c), 0
         elif dir_chars[dir] == 'U':
-            if cur[1] < (face_bounds[1][0][1] + 25):
-                new_r = (face_bounds[6][0][0] + (cur[1] - face_bounds[1][2][1]))
-                new_c = face_bounds[6][0][1]
-                return (new_r,new_c), 0
-            else:
-                new_r = (face_bounds[6][2][0] - (face_bounds[1][1][1] - cur[1]))
-                new_c = face_bounds[6][0][1]
-                return (new_r,new_c), 0
+            diff = (cur[1] - face_bounds[1][0][1])
+            new_r = face_bounds[6][0][0] + diff
+            new_c = face_bounds[6][0][1]
+            return (new_r,new_c), 0
     elif faces[cur] == 2:
         if dir_chars[dir] == 'R':
-            if cur[0] < (face_bounds[2][1][0] + 25):
-                new_r = (face_bounds[5][3][0] - (cur[0] - face_bounds[2][1][0]))
-                new_c = face_bounds[5][1][1]
-                return (new_r,new_c), 2
-            else:
-                new_r = (face_bounds[5][1][0] + (face_bounds[2][3][0] - cur[0]))
-                new_c = face_bounds[5][1][1]
-                return (new_r,new_c), 2
+            diff = face_bounds[2][3][0] - cur[0]
+            new_r = face_bounds[5][1][0] +  diff
+            new_c = face_bounds[5][1][1]
+            return (new_r,new_c), 2
         elif dir_chars[dir] == 'U':
-            if cur[1] < (face_bounds[2][0][1] + 25):
-                new_r = face_bounds[6][2][0]
-                new_c = (face_bounds[6][2][1] + (cur[1] - face_bounds[2][0][1]))
-                return (new_r,new_c), 3
-            else:
-                new_r = face_bounds[6][2][0]
-                new_c = (face_bounds[6][3][1] - (face_bounds[2][1][1] - cur[1]))
-                return (new_r,new_c), 3
+            diff = face_bounds[2][1][1] - cur[1]
+            new_c = face_bounds[6][3][1] - diff
+            new_r = face_bounds[6][3][0]
+            return (new_r,new_c), 3
         elif dir_chars[dir] == 'D':
-            pass
-
+            diff = cur[1] - face_bounds[2][2][1]
+            new_r = face_bounds[3][1][0] + diff
+            new_c = face_bounds[3][1][1]
+            return (new_r,new_c), 2
+    elif faces[cur] == 3:
+        if dir_chars[dir] == 'L':
+            diff = face_bounds[3][2][0] - cur[0]
+            new_c = face_bounds[4][1][1] - diff
+            new_r = face_bounds[4][1][0]
+            return (new_r,new_c), 1
+        elif dir_chars[dir] == 'R':
+            diff = cur[0] - face_bounds[3][1][0]
+            new_c = face_bounds[2][2][1] + diff
+            new_r = face_bounds[2][2][0]
+            return (new_r,new_c),3
+    elif faces[cur] == 4:
+        if dir_chars[dir] == 'U':
+            diff = face_bounds[4][1][1] - cur[1]
+            new_r = face_bounds[3][2][0] - diff
+            new_c = face_bounds[3][2][1]
+            return (new_r,new_c), 0
+        elif dir_chars[dir] == 'L':
+            diff = cur[0] - face_bounds[4][0][0] 
+            new_r = face_bounds[1][2][0] - diff
+            new_c = face_bounds[1][2][1]
+            return (new_r,new_c), 0
+    elif faces[cur] == 5:
+        if dir_chars[dir] == 'R':
+            diff =  cur[0] - face_bounds[5][1][0]
+            new_r = face_bounds[2][3][0] - diff
+            new_c = face_bounds[2][3][1]
+            return (new_r,new_c), 2
+        elif dir_chars[dir] == 'D':
+            diff = cur[1] - face_bounds[5][2][1]
+            new_r = face_bounds[6][1][0] + diff
+            new_c = face_bounds[6][1][1]
+            return (new_r,new_c), 2
+    elif faces[cur] == 6:
+        if dir_chars[dir] == 'R':
+            diff = cur[0] - face_bounds[6][1][0]
+            new_c = face_bounds[5][2][1] + diff
+            new_r = face_bounds[5][2][0]
+            return (new_r,new_c),3
+        elif dir_chars[dir] == 'L':
+            diff = cur[0] - face_bounds[6][0][0]
+            new_c = face_bounds[1][0][1] + diff
+            new_r = face_bounds[1][0][0]
+            return (new_r,new_c),1
+        elif dir_chars[dir] == 'D':
+            diff = face_bounds[6][3][1] - cur[1]
+            new_c = face_bounds[2][1][1] - diff
+            new_r = face_bounds[2][1][0]
+            return (new_r,new_c), 1
 
 def move(steps):
     global dir
@@ -72,11 +107,16 @@ for i in range(len(grid)):
     pad = [' '] * (max_len - len(grid[i]))
     grid[i] = grid[i] + pad
 grid = [[' '] * max_len] + grid + [[' '] * max_len]
-
 # map faces
 faces = defaultdict(int)
 face_bounds = defaultdict(list) # top left top right bottom left bottom right
-for r in range(1,51):
+face_len = 0
+for c in grid[1]:
+    if c != ' ':
+        face_len += 1
+face_len = int(face_len / 2)
+
+for r in range(1,face_len + 1):
     face = 1
     count = 0
     for c in range(len(grid[0])):
@@ -84,16 +124,21 @@ for r in range(1,51):
             continue
         faces[(r,c)] = face
         count += 1
-        if count == 1 and (r == 1 or r == 50):
+        if count == 1 and (r == 1 or r == face_len):
             face_bounds[face].append((r,c))
-        elif count == 50 and (r == 1 or r == 50):
+        elif count == face_len and (r == 1 or r == face_len):
             face_bounds[face].append((r,c))
             if face == 2:
                 break
             count = 0
             face += 1
+        elif count == face_len:
+            if face == 2:
+                break
+            count = 0
+            face += 1
 
-for r in range(51,101):
+for r in range(face_len + 1,2 * face_len + 1):
     face = 3
     count = 0
     for c in range(len(grid[0])):
@@ -101,13 +146,13 @@ for r in range(51,101):
             continue
         faces[(r,c)] = face
         count += 1
-        if count == 1 and (r == 51 or r == 100):
+        if count == 1 and (r == face_len + 1 or r == 2 * face_len):
             face_bounds[face].append((r,c))
-        elif count == 50 and (r == 51 or r == 100):
+        elif count == face_len and (r == face_len + 1 or r == 2 * face_len):
             face_bounds[face].append((r,c))
             break
 
-for r in range(101,151):
+for r in range(2 * face_len + 1,3 * face_len + 1):
     face = 4
     count = 0
     for c in range(len(grid[0])):
@@ -115,16 +160,21 @@ for r in range(101,151):
             continue
         faces[(r,c)] = face
         count += 1
-        if count == 1 and (r == 101 or r == 150):
+        if count == 1 and (r == 2 * face_len + 1 or r == 3 * face_len):
             face_bounds[face].append((r,c))
-        elif count == 50 and (r == 101 or r == 150):
+        elif count == face_len and  (r == 2 * face_len + 1 or r == 3 * face_len):
             face_bounds[face].append((r,c))
             if face == 5:
                 break
             count = 0
             face += 1
+        elif count == face_len:
+            if face == 5:
+                break
+            count = 0
+            face += 1
 
-for r in range(151,201):
+for r in range(3 * face_len + 1,4 * face_len + 1):
     face = 6
     count = 0
     for c in range(len(grid[0])):
@@ -132,9 +182,9 @@ for r in range(151,201):
             continue
         faces[(r,c)] = face
         count += 1
-        if count == 1 and (r == 151 or r == 200):
+        if count == 1 and (r == 3 * face_len + 1 or r == 4 * face_len):
             face_bounds[face].append((r,c))
-        elif count == 50 and  (r == 151 or r == 200):
+        elif count == face_len and  (r == 3 * face_len + 1 or r == 4 * face_len):
             face_bounds[face].append((r,c))
             break
 '''
@@ -144,17 +194,16 @@ for r in range(len(grid)):
 for bounds in face_bounds.values():
     for point in bounds:
         grid[point[0]][point [1]] = 'b'
+grid[64][150] = 'X'
 for r in range(len(grid)):
         print(''.join(grid[r]))
 '''
-
 dirs = [(0,1),(1,0),(0,-1),(-1,0)]
 dir_chars = ['R', 'D', 'L', 'U']
 dir = 0
 delta = {'R' : 1, 'L' : -1}
 
 cur = (1,grid[1].index('.'))
-
 
 for i in range(len(path)):
     if path[i].isnumeric():
@@ -166,9 +215,8 @@ for i in range(len(path)):
 
 row = cur[0] 
 col = cur[1] 
-grid[cur[0]][cur[1]] = 'E'
-for r in range(len(grid)):
-        print(''.join(grid[r]))
+#for r in range(len(grid)):
+#        print(''.join(grid[r]))
 print("row : {}".format(row))
 print("col : {}".format(col))
 print("facing : {}".format(dir))
