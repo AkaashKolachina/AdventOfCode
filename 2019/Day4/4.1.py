707ce15ceeb07e8c973c41aee9d21ebe53c71f87
AkaashKolachina <54248406+AkaashKolachina@users.noreply.github.com>
2024-01-05 04:14:22 -0600

    Finish up to day 4

--- 2019/Day4/4.1.py
+++ 2019/Day4/4.1.py
@@ -2,4 +2,19 @@ import sys
 
 input = sys.argv[1] if len(sys.argv)>1 else 'input.txt'
 data = open(input).read().strip()
-lines = [x for x in data.split('\n')]
+start,end = data.split('-')
+start,end = int(start), int(end)
+
+def is_valid(num):
+    has_adjacent = False
+    for i in range(1,len(num)):
+        if int(num[i]) < int(num[i-1]):
+            return False
+        if num[i] == num[i-1]:
+            has_adjacent = True
+    return has_adjacent
+
+num_possible = 0
+for num in range(start, end + 1):
+    num_possible += 1 if is_valid(str(num)) else 0
+print(num_possible)
