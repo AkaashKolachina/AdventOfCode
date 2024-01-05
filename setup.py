b5ce6469e78e22c5024698c2cfa217a568bf8326
AkaashKolachina <54248406+AkaashKolachina@users.noreply.github.com>
2024-01-04 18:36:58 -0600

    Update setup script and create 2019 files

--- setup.py
+++ setup.py
@@ -1,32 +1,26 @@
+import sys
 import os
-import shutil
-import re
 
-# Create Year Folders 
-years = ["2022", "2023"]
+# Create Year and Day Folders 
+year = sys.argv[1]
 days = ["Day{}".format(i) for i in range(1,26)]
-
-for year in years:
-    os.makedirs(year)
-    for day in days:
-        os.makedirs(os.path.join(year, day))
+os.makedirs(year)
+for day in days:
+    os.makedirs(os.path.join(year, day))
 
 
-# For 2022 move our files into new structure.
-files = [file for file in os.listdir() if os.path.isfile(file)]
-for file in files:
-    if file.strip().split('.')[0].isnumeric():
-        i = int(file.strip().split('.')[0])
-        shutil.move(file, os.path.join("2022", "Day{}".format(i)))
-    elif re.match(r"d(\d+)input\.txt", file.strip()):
-        i = int(re.match(r"d(\d+)input\.txt", file.strip()).group(1))
-        shutil.move(file, os.path.join("2022", "Day{}".format(i)))
-
-# For 2023 Setup python and blank input files
-year = "2023"
+# Setup python and blank input files
 for i in range(1,26):
     day = "Day{}".format(i)
     dir = os.path.join(year, day)
-    files = ["{}.1.py".format(i), "{}.2.py".format(i), "input.txt", "test.txt"]
+    files = ["{}.1.py".format(i), "{}.2.py".format(i), "test.txt"]
     for file in files:
-        open(os.path.join(dir, file), 'a').close() 
+        open(os.path.join(dir, file), 'a').close()
+
+    # Add code to read in input for ever part 1 file 
+    f = open(os.path.join(dir, "{}.1.py".format(i)), "a")
+    f.write('import sys\n\n')
+    f.write("input = sys.argv[1] if len(sys.argv)>1 else 'input.txt'\n")
+    f.write('data = open(input).read().strip()\n')
+    f.write("lines = [x for x in data.split('\\n')]\n")
+    f.close()
