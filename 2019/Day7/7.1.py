c1411fa7ecebde4c75d69167bad31520ae431092
AkaashKolachina <54248406+AkaashKolachina@users.noreply.github.com>
2024-01-06 01:54:53 -0600

    Update Intcode and past solutions

--- 2019/Day7/7.1.py
+++ 2019/Day7/7.1.py
@@ -1,5 +1,7 @@
 import sys
+sys.path.append('..')
+from intcode import run
 
 input = sys.argv[1] if len(sys.argv)>1 else 'input.txt'
 data = open(input).read().strip()
-lines = [x for x in data.split('\n')]
+program = [int(x) for x in data.split(',')]
