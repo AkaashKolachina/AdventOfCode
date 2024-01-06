a26a621217b0134bee1c8b93543c6549f15fb335
AkaashKolachina <54248406+AkaashKolachina@users.noreply.github.com>
2024-01-06 05:56:17 -0600

    Finish Day 7 (kinda jank)

--- 2019/Day7/7.1.py
+++ 2019/Day7/7.1.py
@@ -1,7 +1,32 @@
 import sys
 sys.path.append('..')
-from intcode import run
+from intcode import IntCode
 
 input = sys.argv[1] if len(sys.argv)>1 else 'input.txt'
 data = open(input).read().strip()
-program = [int(x) for x in data.split(',')]
+
+phase_settings = [i for i in range(5)]
+
+permutations = []
+def find_permutations(cur_perm, options):
+    if not options:
+        permutations.append(cur_perm)
+        return
+    for i,option in enumerate(options):
+        find_permutations(cur_perm + [option], options[:i] + options[i+1:])
+
+find_permutations([], phase_settings)
+
+
+max_output = 0
+for perm in permutations:
+    input_signal = 0
+    for i in range(5):
+        program = [int(x) for x in data.split(',')]
+        ic = IntCode(program,[perm[i], input_signal])
+        output = ic.run()
+        while output is not None:
+            input_signal = output
+            output = ic.run()
+    max_output = max(max_output,input_signal)
+print(max_output)
