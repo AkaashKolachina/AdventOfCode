a1545cf1f4a7d80876d90b5b195cbf09bf03d7dc
AkaashKolachina <54248406+AkaashKolachina@users.noreply.github.com>
2024-01-07 02:17:15 -0600

    Finish Day 8

--- 2019/Day8/8.1.py
+++ 2019/Day8/8.1.py
@@ -1,5 +1,10 @@
 import sys
 
 input = sys.argv[1] if len(sys.argv)>1 else 'input.txt'
-data = open(input).read().strip()
-lines = [x for x in data.split('\n')]
+image = open(input).read().strip()
+
+num_rows = 6
+num_cols = 25
+step = num_rows * num_cols
+layer_data = [(image[i:i+step].count('0'),image[i:i+step].count('1') * image[i:i+step].count('2')) for i in range(0,len(image), step)]
+print(min(layer_data)[1])
