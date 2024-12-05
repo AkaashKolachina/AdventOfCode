cb8f4b7e2606436c345a778c937774344cc5a349
AkaashKolachina <54248406+AkaashKolachina@users.noreply.github.com>
2024-12-04 23:22:30 -0600

    Add quick implementation for part 5

---
+++ 2024/Day5/5.1.py
@@ -0,0 +1,30 @@
+import sys
+from collections import defaultdict
+
+input = sys.argv[1] if len(sys.argv)>1 else 'input.txt'
+data = open(input).read().strip()
+lines = [x for x in data.split('\n')]
+rules = defaultdict(set)
+pages = []
+for line in lines:
+    if line == '':
+        continue
+    if '|' in line:
+        before,after = map(int,line.split('|'))
+        rules[after].add(before)
+    else:
+        pages.append([int(x) for x in line.split(',')])
+
+total = 0
+for page in pages:
+    is_right = True
+    for i in range(len(page)):
+        for j in range(i, len(page)):
+            if page[j] in rules[page[i]]:
+                is_right = False
+                break
+        if not is_right:
+            break
+    if is_right:
+        total += page[len(page) // 2]
+print(total)
