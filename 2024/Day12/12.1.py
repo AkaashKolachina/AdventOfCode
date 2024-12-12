5a5efbe838960aea080a53d217af07b9a46fd9f5
AkaashKolachina <54248406+AkaashKolachina@users.noreply.github.com>
2024-12-12 01:31:24 -0600

    Finish Day 12

--- 2024/Day12/12.1.py
+++ 2024/Day12/12.1.py
@@ -1,5 +1,54 @@
 import sys
+from collections import deque
 
 input = sys.argv[1] if len(sys.argv)>1 else 'input.txt'
 data = open(input).read().strip()
-lines = [x for x in data.split('\n')]
+grid = [list(x) for x in data.split('\n')]
+
+regions = set()
+visited = set()
+def get_neighbors(i,j):
+    nbs = []
+    dirs = [(0,1), (1,0), (-1,0), (0,-1)]
+    for di,dj in dirs:
+        if 0 <= i + di < len(grid) and 0 <= j + dj < len(grid[0]):
+            nbs.append((i+di,j+dj))
+    return nbs
+
+def bfs(i,j):
+    region = set()
+    q = deque([(i,j)])
+    while len(q):
+        i,j = q.pop()
+        region.add((i,j))
+
+        for nb in get_neighbors(i,j):
+            if nb not in visited and grid[nb[0]][nb[1]] == grid[i][j]:
+                q.appendleft(nb)
+                visited.add(nb)
+    return frozenset(region)
+
+for i in range(len(grid)):
+    for j in range(len(grid[0])):
+        if (i,j) not in visited:
+            visited.add((i,j))
+            regions.add(bfs(i,j))
+
+price = 0
+for region in regions:
+    area = len(region)
+    perimeter = 0
+    for i,j in region:
+        if (i, j+1) not in region:
+            perimeter += 1
+        if (i, j-1) not in region:
+            perimeter += 1
+        if (i+1, j) not in region:
+            perimeter += 1
+        if (i-1,j) not in region:
+            perimeter += 1
+    price += (area*perimeter)
+print(price)
+
+
+
